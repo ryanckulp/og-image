ee54222daf7c132fbe0e0bb9e010418fd947abdf
Ryan Kulp <ryanckulp@gmail.com>
2020-04-14 16:35:10 +0900

    attempt to encode hangul inside <code>

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -120,7 +120,7 @@ export function getHtml(parsedReq: ParsedRequest) {
                 ).join('')}
             </div>
             <div class="spacer">
-            <div class="heading">test-시험-test
+            <div class="heading">test-<code>시험</code>-test
                 ${emojify(
                     md ? marked(text) : sanitizeHtml(text)
                 )}
