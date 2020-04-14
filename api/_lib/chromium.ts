494152a8eb99e80713caa82ed60743e0cdafc718
Ryan Kulp <ryanckulp@gmail.com>
2020-04-14 22:15:29 +0900

    try another path

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -38,7 +38,7 @@ export async function getScreenshot(html: string, type: FileType, isDev: boolean
     await console.dir(options);
 
     // EXPERIMENT - delete all 3 lines
-    var path = '/usr/local/bin/chromedriver'; // require('chromedriver').path;
+    var path = '/tmp/chromium'; // require('chromedriver').path;
     var service = new chrome.ServiceBuilder(path).build();
     chrome.setDefaultService(service);
 
