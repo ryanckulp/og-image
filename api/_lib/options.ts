457524671a5f1310e54048d923bd8b00c602cabb
Ryan Kulp <ryanckulp@gmail.com>
2020-04-14 15:30:28 +0900

    begin chrome.args elimination diet

--- api/_lib/options.ts
+++ api/_lib/options.ts
@@ -11,9 +11,57 @@ interface Options {
 
 export async function getOptions(isDev: boolean) {
     let options: Options;
+
+    var customArgs = [
+       '--disable-background-timer-throttling',
+       '--disable-breakpad',
+       '--disable-client-side-phishing-detection',
+       '--disable-cloud-import',
+       '--disable-default-apps',
+       '--disable-dev-shm-usage',
+       '--disable-extensions',
+       '--disable-gesture-typing',
+       '--disable-hang-monitor',
+       '--disable-infobars',
+       '--disable-notifications',
+       '--disable-offer-store-unmasked-wallet-cards',
+       '--disable-offer-upload-credit-cards',
+       '--disable-popup-blocking',
+       '--disable-print-preview',
+       '--disable-prompt-on-repost',
+       '--disable-setuid-sandbox',
+       '--disable-speech-api',
+       '--disable-sync',
+       '--disable-tab-for-desktop-share',
+       '--disable-translate',
+       '--disable-voice-input',
+       '--disable-wake-on-wifi',
+       '--disk-cache-size=33554432',
+       // '--enable-async-dns',
+       // '--enable-simple-cache-backend',
+       // '--enable-tcp-fast-open',
+       // '--enable-webgl',
+       // '--hide-scrollbars',
+       // '--ignore-gpu-blacklist',
+       // '--media-cache-size=33554432',
+       // '--metrics-recording-only',
+       // '--mute-audio',
+       // '--no-default-browser-check',
+       // '--no-first-run',
+       // '--no-pings',
+       // '--no-sandbox',
+       // '--no-zygote',
+       // '--password-store=basic',
+       // '--prerender-from-omnibox=disabled',
+       // '--use-gl=swiftshader',
+       // '--use-mock-keychain',
+       // '--memory-pressure-off',
+       // '--single-process'
+    ]
+
     if (isDev) {
         options = {
-            args: ['--disable-web-security'],
+            args: customArgs, // was [], then ['--disable-web-security']
             executablePath: exePath,
             headless: false // was true
         };
@@ -22,7 +70,7 @@ export async function getOptions(isDev: boolean) {
         // newArgs.push('--disable-web-security'); // added to render hangul in prod
 
         options = {
-            args: ['--disable-web-security'], // before: chrome.args (maybe args changing font render)
+            args: customArgs, // before: chrome.args
             executablePath: await chrome.executablePath,
             headless: false, // was chrome.headless
         };
