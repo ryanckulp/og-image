5eb15cf904e9dab2e2cd248a5109fac27b930eda
Ryan Kulp <ryanckulp@gmail.com>
2020-04-14 14:26:02 +0900

    await document.fonts.ready earlier in handler

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -18,9 +18,9 @@ async function getPage(isDev: boolean) {
 
 export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
     const page = await getPage(isDev);
+    await page.evaluateHandle('document.fonts.ready');
     await page.setViewport({ width: 2048, height: 1170 });
     await page.setContent(html);
-    await page.evaluateHandle('document.fonts.ready');
     // await delay(2000); // check if font loads in prod
     const file = await page.screenshot({ type });
     return file;
